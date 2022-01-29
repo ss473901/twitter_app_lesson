346d0cfb1ca4b9ec5dd44be9742e3b38ca833694
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-01-29 14:56:18 +0900

    Add Style

--- src/ui/page/home/Feed.tsx
+++ src/ui/page/home/Feed.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { auth } from "../../../config/firebase";
-import { Header, Footer, Card, Grid } from "../../../design/atoms/index";
 import Button from "@mui/material/Button";
 
 const Feed: React.FC = () => {
   return (
     <>
       <Button onClick={() => auth.signOut()}>Logout</Button>
-      <Grid>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </Grid>
-      <Footer />
     </>
   );
 };
